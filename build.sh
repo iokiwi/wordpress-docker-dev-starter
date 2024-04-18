0a948b1a2f8a5e05cea78f731bd7d72bec6da9ae
Simon Merrick <s.m3rrick@gmail.com>
2024-04-18 12:10:40 +1200

    A lot more automation. Too much to list

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 WORDPRESS_TAG=${1:-"6.4-php8.3"}
 
+echo "\$WORDPRESS_TAG=$WORDPRESS_TAG"
+
 docker compose build \
   --build-arg WORDPRESS_TAG="${WORDPRESS_TAG}" \
   --build-arg USER_ID=$(id -u) \
